b24ee46b9cb320014f30c3c2e5bf2b70085dded6
sosna <gemechusosi@gmail.com>
2025-07-09 12:14:22 +0300

    Change output format to math expression style

--- basic_operations.py
+++ basic_operations.py
@@ -5,6 +5,7 @@ addition = number1 + number2
 subtraction = number1 - number2
 multiplication = number1 * number2
 
-print(f"Addition of {number1} and {number2} is {addition}")
-print(f"Subtraction of {number1} and {number2} is {subtraction}")
-print(f"Multiplication of {number1} and {number2} is {multiplication}")
+print("Addition of", number1, "and", number2, "is", addition)
+print("Subtraction of", number1, "and", number2, "is", subtraction)
+print("Multiplication of", number1, "and", number2, "is", multiplication)
+
