bd9350b205dedd9d8e95871bf3c09ddec4da8ffa
sosna <gemechusosi@gmail.com>
2025-07-09 11:58:23 +0300

    perform basic operations on two integer numbers

---
+++ basic_operations.py
@@ -0,0 +1,5 @@
+number1 =20
+number2 =30
+print("Addition of", number1 ,"and",number2," is",number1+number2)
+print("Subtraction of", number1 ,"from",number2," is",number2-number1)
+print("Multiplication of", number1 ,"and",number2," is",number1*number2)
