95c50b6228f166f122dd9d715cf1c1304da6883c
sosna <gemechusosi@gmail.com>
2025-08-16 19:10:33 +0300

    push project directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+print("Addition of", number1, "and", number2, "is", sum_result)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
+ 
---
+++ rectangle.area.py
@@ -0,0 +1,4 @@
+width = 5
+height = 10
+area = width * height
+print("The area of the rectangle is:", area)
